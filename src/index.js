56c5a7e24f45a63966e2a10adab561b98b1a47f2
Ritesh <riteshy2k2010@gmail.com>
2021-03-29 12:33:40 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import App from './src/App'
+
+// ReactDOM.rendor('Kya dikhana hai', 'kaha dikhana ' , 'callback function');
+ReactDOM.render(<App/>, document.getElementById('root') );
